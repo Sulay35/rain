fc73132f65f1bd7bbb2e34552ce760aadf687951
Sulay35 <sulaymane.dagnet@laposte.net>
2020-02-02 17:34:14 +0100

    new sparks system

--- demo.js
+++ demo.js
@@ -16,6 +16,9 @@ var gouttes = 1;
 var circleArray = [];
 var sparksArray = [];
 
+// Rain mode
+var autoRain = false;
+
 // Functions :
 function random_int(min, max){
     return Math.floor(Math.random()*(max-min+1) + min);
@@ -35,12 +38,20 @@ class Sparks {
         this.dy = dy;
         this.gravity = 0.5;
 		this.gravitySpeed = 1;
+		this.hasExploded = false;
 
+		this.explode = function(){
+			for (var i = 0; i< random_int(3,6); i++){
+				let YSparksspeed = random_float(-8,-3);
+				let XSparksspeed = random_float(-2,2);
+				sparksArray.push(new Sparks(this.x ,this.y ,XSparksspeed,YSparksspeed,this.radius*0.45));
+			}
+			this.hasExploded = true;
+		}
 		
 		this.draw = function () {
 			c.fillStyle = "#0099ff";
 			c.beginPath();
-			
 			c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
 			c.fill();
 			
@@ -51,6 +62,11 @@ class Sparks {
 			}
 			if (this.y + this.radius >= innerHeight) {
 				this.dy = -this.dy;
+				if(this.radius > 0.5){
+					if(!this.hasExploded){
+						this.explode();
+					}
+				}
             }
             this.gravitySpeed += this.gravity;
 			this.x += this.dx;
@@ -104,7 +120,7 @@ class Drop {
 					for (var i = 0; i< 3; i++){
 						sparksArray.push(new Sparks(this.x ,this.y ,-5 ,0.2,this.radius*0.45));
 					}
-					this.hasExploded = true
+					this.hasExploded = true;
 				}
 			}
 
@@ -128,12 +144,12 @@ class Drop {
 
 function rain(){
     for(var i =0; i<num;i++){
-        var radius = 5;
+        var radius = random_float(1,3);
         //var x = Math.random()*((innerWidth/2+20)-(innerWidth/2-20))+(innerWidth/2-20) ;
         var x = Math.random()*(innerWidth-5)-5
-        var y = Math.random()*(-50+3)+3;
+        var y = random_int(-20,-10)
         var dx = Math.random()*0.7;
-        var dy = Math.random()*(5-2)+2;
+        var dy = Math.random()*(15-2)+2;
         
         circleArray.push(new Drop(x ,y ,dx ,dy ,radius ));
         
@@ -153,41 +169,57 @@ function rain(){
 var longpress = false;
 var mouseX = 0;
 var mouseY = 0;
+var size = 0;
+var rayon = 2;
 canvas.addEventListener('mousedown', (event) => {
+	event.preventDefault()
 	mouseX = event.offsetX;
 	mouseY = event.offsetY;
 	longpress = true;
 });
-
-// wheel control size :
-canvas.addEventListener('wheel', (event) => {
-	var size = Math.min(Math.max(.125, event.deltaY),-0.125);
-	console.log('SIZE : ', size)
-})
-
 canvas.addEventListener('mouseup', (event) => {
+	event.preventDefault()
+
 	longpress = false;
 })
 
-//setInterval(rain,500)
+// Rain mode change :
+var raining;
+document.addEventListener('keypress', (event) => {
+	if(!autoRain){
+		autoRain = true;
+		raining = setInterval(rain,500);
+	}else{
+		autoRain = false;
+		clearInterval(raining);
+	}
+
+})
+
+
+var taille = 0;
 
 function animate(){
 	requestAnimationFrame(animate);
 	c.clearRect(0,0,innerWidth,innerHeight);
 
+	// Drops
 	for(var i = 0; i< circleArray.length ;i++){
-		
-		if(circleArray[i].hasExploded){
-			circleArray.splice(i,1);
-		}
-		if(circleArray[i].y >= innerHeight){
-			circleArray.splice(i, 1);
-		}
-		else{
-			circleArray[i].update();
+		if(circleArray[i] != undefined){
+			if(circleArray[i].hasExploded){
+				circleArray.splice(i,1);
+			}
+			if(circleArray[i] != undefined){
+				if(circleArray[i].y >= innerHeight){
+					circleArray.splice(i, 1);
+				}
+				else{
+					circleArray[i].update();
+				}
+			}
 		}
 	}
-	
+	// Sparks
 	for(var i = 0; i < sparksArray.length; i++){
 		if(sparksArray[i].y >= innerHeight){
 			sparksArray.splice(i, 1);
@@ -196,6 +228,20 @@ function animate(){
 		}
 		
 	}
+	
+	// Mouse
+	canvas.addEventListener('wheel', (event) => {
+		c.fillStyle = '#FFFFFF';
+		c.beginPath();
+		c.arc(mouseX, mouseY, 5, 0, Math.PI * 2, false);
+		c.strokeStyle = '#FF0000';
+		c.fill();
+		c.stroke();
+
+		size = event.deltaY/100;
+		taille = rayon-size;
+		//console.log(event)
+	})
 
 	if(longpress){
 		// drag and drop drops
@@ -203,14 +249,16 @@ function animate(){
 			mouseX = event.offsetX;
 			mouseY = event.offsetY;
 		})
+		
 		for (var i = 0; i < 2; i++){
-			var dx = random_float(-0.1,0.1);
-			var dy = random_float(-0.5,0.5)
-			circleArray.push(new Drop(mouseX, mouseY, dx, dy, 2))
+			var dx = random_float(-0.2,0.1);
+			var dy = random_float(-0.5,5)
+			
+			rayon = Math.max(1,taille);
+			console.log(rayon)
+			circleArray.push(new Drop(mouseX, mouseY, dx, dy, rayon))
 		}
 	}
-
-	
 }
 
 animate();
